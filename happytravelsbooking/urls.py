48c33273cd7727a4bc0c78208dd244f0e8d34c97
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-11 02:40:00 +0000

    change home to homepage on base.html url section

--- happytravelsbooking/urls.py
+++ happytravelsbooking/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from booking import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
     path('accounts/', include('allauth.urls')),
 ]
