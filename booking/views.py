ee17dea9a8383489064571b4f149c11a40d3cb40
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-16 11:11:19 +0000

    input form data onto appointment.html page

--- booking/views.py
+++ booking/views.py
@@ -11,25 +11,24 @@ def booking(request):
     return render(request, 'form.html')
 
 def appointment(request):
-    return render(request, 'appointment.html', {})
-
- 	if request.method == "POST":
- 		your_name = request.POST['your-name']
- 		your_phone = request.POST['your-phone']
- 		your_email = request.POST['your-email']
- 		your_address = request.POST['your-address']
- 		your_schedule = request.POST['your-schedule']
- 		your_date = request.POST['your-date']
- 		your_message = request.POST['your-message']
-		
-# 		return render(request, 'appointment.html', {
-# 			'your_name': your_name,
-# 			'your_phone': your_phone,
-# 			'your_email': your_email,
-# 			'your_address': your_address,
-# 			'your_schedule': your_schedule,
-# 			'your_date': your_date,
-# 			'your_message': your_message
-# 			})
-# 	else:
-# 		return render(request, 'home.html', {})
+    if request.method== "POST" :
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+        
+        return render(request, 'appointment.html', {
+            'your_name': your_name,
+ 			'your_phone': your_phone,
+ 			'your_email': your_email,
+ 			'your_address': your_address,
+ 			'your_schedule': your_schedule,
+ 			'your_date': your_date,
+ 			'your_message': your_message
+ 			}) 
+    
+    else:
+        return render(request, 'home.html', {})
