594f0fc2c457f8b1bed56b6b66bdf5ce9d1f16e0
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-16 10:46:35 +0000

    add form settings to view.py

--- booking/views.py
+++ booking/views.py
@@ -13,24 +13,14 @@ def booking(request):
 def appointment(request):
     return render(request, 'appointment.html', {})
 
-# 	if request.method == "POST":
-# 		your_name = request.POST['your-name']
-# 		your_phone = request.POST['your-phone']
-# 		your_email = request.POST['your-email']
-# 		your_address = request.POST['your-address']
-# 		your_schedule = request.POST['your-schedule']
-# 		your_date = request.POST['your-date']
-# 		your_message = request.POST['your-message']
-		
-# 		# send an email
-# 		appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Schedule: " + your_schedule + " Day: " + your_date + " Message: " + your_message
-
-# 		send_mail(
-# 			'Appointment Request', # subject
-# 			appointment, # message
-# 			your_email, # from email
-# 			['john@codemy.com'], # To Email
-# 			)
+ 	if request.method == "POST":
+ 		your_name = request.POST['your-name']
+ 		your_phone = request.POST['your-phone']
+ 		your_email = request.POST['your-email']
+ 		your_address = request.POST['your-address']
+ 		your_schedule = request.POST['your-schedule']
+ 		your_date = request.POST['your-date']
+ 		your_message = request.POST['your-message']
 		
 # 		return render(request, 'appointment.html', {
 # 			'your_name': your_name,
