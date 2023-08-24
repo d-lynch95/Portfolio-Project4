ff37cc7ef89ac4273b88707ea893417fdfaa4c46
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-24 12:30:49 +0000

    change location of the base files

--- booking/models.py
+++ booking/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from . import views
 
 class Post(models.Model):
     name = models.CharField(max_length=50)
@@ -9,4 +10,15 @@ class Post(models.Model):
     email = models.EmailField()
     time = models.TimeField()
     date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
     # need to add appointment Id and user information
+
+# This class orders the appointments in reverse order
+class Meta:
+    ordering = ['-created_on']
+
+def __str__(self):
+    return self.title
+
+
--- booking/views.py
+++ booking/views.py
@@ -2,7 +2,3 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, UpdateView, CreateView
-
-class MyCreateView(CreateView):
-    model = booking
-    form_class = MyForm
