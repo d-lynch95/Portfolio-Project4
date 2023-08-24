f5d586f9e8decc0d0ed23029d68227eb07e2571f
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-24 12:16:28 +0000

    add Post model to models.py file

--- booking/models.py
+++ booking/models.py
@@ -3,3 +3,10 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    time = models.TimeField()
+    date = models.DateField()
+    # need to add appointment Id and user information
