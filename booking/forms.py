60c6461dc6eca55295baf457c74a4c16af778f3c
d-lynch95 <darraghlynch1995@gmail.com>
2023-08-27 09:51:30 +0000

    add ApptForm to forms.py

--- booking/forms.py
+++ booking/forms.py
@@ -1,2 +1,7 @@
 from django import forms
+from .models import Post
 
+class ApptForm(forms.ModelForm):
+    class Meta:
+    model = Post
+    fields = ['name', 'phone', 'email', 'time', 'date', 'user', 'slug']
