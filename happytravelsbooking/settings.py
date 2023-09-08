6be08168d60113121f25a65ac6512c9de938707b
d-lynch95 <darraghlynch1995@gmail.com>
2023-09-08 07:31:33 +0000

    set DEBUG to false

--- happytravelsbooking/settings.py
+++ happytravelsbooking/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates//')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-dlynch95-portfolioproje-n6ccfcwpcgq.ws-us104.gitpod.io', 'happy-travels-appt-booking-469f488c0638.herokuapp.com']
 
