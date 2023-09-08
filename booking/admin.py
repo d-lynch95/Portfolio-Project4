cad9e984c0ebe0fd76c89d18ec5eebc183984249
d-lynch95 <darraghlynch1995@gmail.com>
2023-09-08 13:19:31 +0000

    update app and admin files with corrections from python linter

--- booking/admin.py
+++ booking/admin.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_filter = ('name','date','time')
-    search_fields = ['name', 'date', 'time', 'phone', 'email',]
-    list_display = ('name', 'date', 'time', 'phone', 'email',)
-
-
+    list_filter = ('name', 'date', 'time')
+    search_fields = ['name', 'date', 'time', 'phone', 'email', ]
+    list_display = ('name', 'date', 'time', 'phone', 'email', )
