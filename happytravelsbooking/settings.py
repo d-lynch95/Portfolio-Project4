0d2e5015d39fb31721816866b93cd605642edda9
d-lynch95 <darraghlynch1995@gmail.com>
2023-09-08 09:58:37 +0000

    add cloudinary_storage to settings.py file

--- happytravelsbooking/settings.py
+++ happytravelsbooking/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'booking',
